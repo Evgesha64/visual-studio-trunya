320ab70cdd50bddad3aad8b487a7efca3812e09b
Женя Войняк <vojnyak02@mail.ru>
2023-11-20 11:16:05 +0300

    Функция для копирования динамическово массива.

--- lesson_1/lesson_1.cpp
+++ lesson_1/lesson_1.cpp
@@ -18,12 +18,33 @@ void arrPublic(int* const arr, const int size)
 	}
 }
 
+void arrCopi(int* arr1, int* const arr2,const int size)
+{
+	delete[] arr1;
+	arr1 = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		arr1[i] = arr2[i];
+
+	}
+}
+
 int main()
 {
 	int size = 10;
 	int* MyArr1 = new int(size);
 	int* MyArr2 = new int(size);
-	arrGenerstor(MyArr1,size);
+	
+	arrGenerstor(MyArr1, size);
+	arrGenerstor(MyArr2, size);
 	arrPublic(MyArr1, size);
+	arrPublic(MyArr2, size);
 
+	
+	//arrCopi(MyArr1, MyArr2, size);
+
+	//arrPublic(MyArr2, size);
+
+	delete[] MyArr1;
+	delete[] MyArr2;
 }
