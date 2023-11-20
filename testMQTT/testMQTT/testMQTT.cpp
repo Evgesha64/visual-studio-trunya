2a85230a06be0f789996ce04c5e3626a2a95355c
Женя Войняк <vojnyak02@mail.ru>
2023-11-20 09:45:04 +0300

    Добавьте файлы проекта.

---
+++ testMQTT/testMQTT/testMQTT.cpp
@@ -0,0 +1,24 @@
+﻿#include <mosquittopp.h>
+#include <iostream>
+
+
+
+int main() {
+    
+    libmosq_EXPORT int mosquitto_lib_init();
+    
+    std::cout << MOSQ_ERR_SUCCESS;
+    std::cout << MOSQ_ERR_UNKNOWN;
+    libmosq_EXPORT struct mosquitto* mosquitto_new(const 	char* id,
+        bool 		clean_session,
+        void* obj);
+
+
+
+
+
+
+
+
+    return 0;
+}
