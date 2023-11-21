3aabb1a64b2f8e985c6ec4ef9d2720201c8d387b
Женя Войняк <vojnyak02@mail.ru>
2023-11-21 13:22:49 +0300

    Добавил 2 урока.

---
+++ Stroki_trenya/Stroki_trenya.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <string>
+#include <iostream>
+ using namespace std;
+
+ void strizmen(char *str) {
+
+	 str[2] = 'f';
+ }
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	char simvol_arr[] = {'H','e','l','l','o','\0'};
+	cout << strlen(simvol_arr) << "\n";
+	cout << simvol_arr << "\n";
+	int d = -33;
+	string str1[5];
+	str1[0] = "Привет";
+	str1[1] = "b";
+	str1[2] = "c";
+	string str2 = "мир";
+	string str3 = str1[0] + " " + str2 + str1[1]+ str1[2];
+	cout << str3 << "\n";
+}
+/*for (int i = 0; i < 255; i++)
+	{
+		cout << "code = " << i << "\tchar = " << (char)i << endl;
+	}
+	char str0[255]{} ;
+	char str1[255] = "Hello";
+	char str2[255] = "Hello";
+	cout << str1 << endl;
+	strizmen(str1);
+    cout << str1 << endl;
+    strcat(str0, str1);
+	strcat(str0, str2);
+    cout << str0 << endl;*/
---
+++ Ukazateli_na_funkciu/Ukazateli_na_funkciu.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+void foo1(void *a) {
+
+    //cout << "foo1 = "<< a << endl ;
+     *a *2;
+}
+
+void foo2(void *a) {
+
+    //cout << "foo2 = "<< a;
+     *a * 2;
+}
+
+
+
+    int main()
+{
+        float* a;
+        *a = 3.24535;
+       void(*voidPointor[2])(void *a);
+         voidPointor[0] = foo1;
+         cout << voidPointor[0](a) << endl;
+         voidPointor[1] = foo2;
+         cout << voidPointor[1](a) << endl;
+
+} 
