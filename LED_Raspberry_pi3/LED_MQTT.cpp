f3483ab75ef5d5873999dad176159da9f5a72057
Женя Войняк <vojnyak02@mail.ru>
2024-02-02 10:09:37 +0300

    Реализована проверка топика

--- LED_Raspberry_pi3/LED_MQTT.cpp
+++ LED_Raspberry_pi3/LED_MQTT.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>     //Стандартная библиотека ввода-вывода в C и C++.
 #include <stdlib.h>    //Библиотека языка C, которая содержит функции для выполнения различных операций, включая обработку строк, математические вычисления, управление памятью, обработку окружения и другие.
 #include <string.h>    //Библиотека которая содержит функции для обработки строк.
+
 #include <cstring>     //Библиотека которая содержит функции для работы со строками и блоками памяти.
 #include <iostream>    //Библиотека которая включает в программу стандартную библиотеку ввода-вывода.
 #include <thread>      //Библиотека предоставляет функции и классы, которые позволяют создавать и управлять потоками выполнения в программе.
@@ -11,8 +12,10 @@ extern "C" {           //"extern" в C++ используется для указания компилятору, ч
 
 #define MQTT_HOSTNAME "192.168.1.3"   // IP адрес брокера.
 #define MQTT_PORT 1883                  // Порт брокера.    
-#define MQTT_TOPIC "topic_num_RGB"      // Топик для приёма изображения от брокера.
-#define MQTT_TOPIC2 "topicCallback"     // Топик для обратной связи. 
+#define MQTT_TOPIC1 "topic_num_RGB"      // Топик для приёма изображения от брокера.
+#define MQTT_TOPIC2 "topic_num_RGB2" 
+#define MQTT_TOPIC3 "topic_stop" 
+#define MQTT_TOPIC4 "topicCallback"     // Топик для обратной связи. 
 
 #define TARGET_FREQ             WS2811_TARGET_FREQ // Устанавливаем целевую частоту для светодиодной ленты WS2811.
 #define GPIO_PIN                18                 // Устанавливаем номер GPIO (General Purpose Input/Output) пина.
@@ -49,7 +52,9 @@ ws2811_t ledstring =
  // Функция on_connect вызывается, когда клиент MQTT подключается к брокеру MQTT.
 void on_connect(struct mosquitto* mosq, void* obj, int reason_code)
 {
-    int rc = mosquitto_subscribe(mosq, NULL, MQTT_TOPIC, 1);
+    int rc = mosquitto_subscribe(mosq, NULL, MQTT_TOPIC1, 1);
+    rc = mosquitto_subscribe(mosq, NULL, MQTT_TOPIC2, 1);
+    rc = mosquitto_subscribe(mosq, NULL, MQTT_TOPIC3, 1);
     if (rc != MOSQ_ERR_SUCCESS) {
         fprintf(stderr, "Error subscribing to topic: %s\n", mosquitto_strerror(rc));
         mosquitto_disconnect(mosq);
@@ -58,33 +63,43 @@ void on_connect(struct mosquitto* mosq, void* obj, int reason_code)
 
 void on_message(struct mosquitto* mosq, void* obj, const struct mosquitto_message* message)
 {
-        //Проверяем, соответствует ли длина полученного сообщения ожидаемому размеру
-        if (message->payloadlen != LED_COUNT * sizeof(uint32_t)) {
-            std::cout << "12345";
-            std::cout << "Incorrect message size  " << message->payloadlen << " a nado " << LED_COUNT * sizeof(uint32_t) << std::endl;
+    
+        if (std::string(message->topic) == "topic_num_RGB") {
+            //Проверяем, соответствует ли длина полученного сообщения ожидаемому размеру
+            if (message->payloadlen != LED_COUNT * sizeof(uint32_t)) {
+                std::cout << "12345";
+                std::cout << "Incorrect message size  " << message->payloadlen << " a nado " << LED_COUNT * sizeof(uint32_t) << std::endl;
 
-            char msg[256]; // Буфер для форматированной строки. 
-            snprintf(msg, sizeof(msg), "Incorrect message size %d a nado %lu", message->payloadlen, LED_COUNT * sizeof(uint32_t));
+                char msg[256]; // Буфер для форматированной строки. 
+                snprintf(msg, sizeof(msg), "Incorrect message size %d a nado %lu", message->payloadlen, LED_COUNT * sizeof(uint32_t));
+                message_publish(mosq, msg);
+                return;
+
+            }
+
+            std::memcpy(ledstring.channel[0].leds, message->payload, LED_COUNT * sizeof(uint32_t));
+            std::memcpy(ledstring.channel[1].leds, message->payload, LED_COUNT * sizeof(uint32_t));
+            ws2811_render(&ledstring);
+
+            char* msg = "1";
             message_publish(mosq, msg);
-            return;
-
+   
+        }else if(std::string(message->topic) == "topic_stop")
+        {
+            char* msg = "MQTT_LED Stop";
+            message_publish(mosq, msg);
+            exit(0);
+            
         }
-
-       // char* msg = "1";
-       // message_publish(mosq, msg);
-
-        std::memcpy(ledstring.channel[0].leds, message->payload, LED_COUNT * sizeof(uint32_t));
-        std::memcpy(ledstring.channel[1].leds, message->payload, LED_COUNT * sizeof(uint32_t));
-        ws2811_render(&ledstring);
-        
-       
-       // char* msg2 = "2";
-        //message_publish(mosq,msg2);
-        
-  
+        else
+        {
+             char* msg = "0";
+             message_publish(mosq,msg);
+        }
+                
 }
 void message_publish(struct mosquitto* mosq, const char* msg) {
-    int ret = mosquitto_publish(mosq, NULL, MQTT_TOPIC2, strlen(msg), msg, 0, false);
+    int ret = mosquitto_publish(mosq, NULL, MQTT_TOPIC4, strlen(msg), msg, 0, false);
     if (ret != MOSQ_ERR_SUCCESS) {
         std::cout << "Error: Could not publish message. %s\n"<< mosquitto_strerror(ret);
     }
@@ -171,7 +186,7 @@ int main()
         return 1;
     }*/
     //running_fire();
-   // ws2811_render(&ledstring);
+    // ws2811_render(&ledstring);
     
      mosquitto_loop_forever(mosq, 0, 1);
      mosquitto_destroy(mosq);
